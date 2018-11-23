37d5c6a61bd389a824bf058ca0473d844272eb77
Jaime Carrasquero <desarrollo@tezio.com.co>
2018-11-23 12:25:44 -0500

    Moficacion Tipo
    Moficacion Tipo

--- wsJSONRegistroMovilc.php
+++ wsJSONRegistroMovilc.php
@@ -6,15 +6,15 @@ $password_localhost="Tezio159sA";
 
 	$conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 	
-	@mysqli_query("SET NAMES 'utf8'",$conexion);
+	//@mysqli_query("SET NAMES 'utf8'",$conexion);
 
 	$fechareg=$_POST['fechareg'];
 	$intvercli=$_POST['intvercli'];
 	$intvercal=$_POST['intvercal'];
 	$intvercox=$_POST['intvercox'];
 	$intverfun=$_POST['intverfun'];
-	$strobs=$_POST['strobs'];
-	$strdis=$_POST['strdis'];
+	$strobs=html_entity_decode($_POST['strobs'], ENT_QUOTES | ENT_HTML401, "UTF-8");
+	$strdis=html_entity_decode($_POST['strdis'], ENT_QUOTES | ENT_HTML401, "UTF-8");
 	$estatus='2';
 
 	$insert="INSERT INTO cicloagua(fechareg, intvercli, intvercal, intvercox, intverfun, strobs, strdis, est_cicloagua) VALUES ('{$fechareg}', '{$intvercli}', '{$intvercal}', '{$intvercox}', '{$intverfun}', '{$strobs}', '{$strdis}', '{$estatus}')";
